c7126d79fda2d1fcc1370ab68464a75581553665
SofiaShperling <73811581+SofiaShperling@users.noreply.github.com>
2021-06-25 17:33:26 +0700

    Add files via upload

---
+++ writersociety/Root/public/content.php
@@ -0,0 +1,83 @@
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Writer Society</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="header-wrapper">
+<div id="header">
+    <div id="menu">
+        <ul>
+            <li class="current_page_item"><a href="#">Main page</a></li>
+            <li><a href="#">Posts</a></li>
+            <li><a href="#">Authors</a></li>
+            <li><a href="#" class="last">Personal page</a></li>
+        </ul>
+    </div>
+    <!-- end #menu -->
+
+</div>
+</div>
+<!-- end #header -->
+<!-- end #header-wrapper -->
+
+<hr />
+<!-- end #logo -->
+<div id="page">
+    <div id="page-bgtop">
+        <div id="content">
+            <div class="post">
+                <p class="meta"><span class="date">Sunday, April 26, 2009</span> 7:27 AM Posted by <a href="#">Someone</a></p>
+                <h2 class="title"><a href="#">Welcome to Predilection </a></h2>
+                <div class="entry">
+                    <p>This is <strong>Predilection </strong>, a free, fully standards-compliant CSS template designed by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>, released for free under the <a href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5</a> license.  You're free to use this template for anything as long as you link back to <a href="http://www.freecsstemplates.org/">my site</a>. Enjoy :)</p>
+                    <p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in felis. </p>
+                </div>
+            </div>
+            <div class="post">
+                <p class="meta"><span class="date">Sunday, April 26, 2009</span> 7:27 AM Posted by <a href="#">Someone</a></p>
+                <h2 class="title"><a href="#">Lorem ipsum sed aliquam</a></h2>
+                <div class="entry">
+                    <p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec   consectetuer. Donec ipsum. Proin imperdiet est. Phasellus <a href="#">dapibus semper urna</a>. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros.</p>
+                </div>
+            </div>
+            <div class="post">
+                <p class="meta"><span class="date">Sunday, April 26, 2009</span> 7:27 AM Posted by <a href="#">Someone</a></p>
+                <h2 class="title"><a href="#">Lorem ipsum sed aliquam</a></h2>
+                <div class="entry">
+                    <p>Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus <a href="#">dapibus semper urna</a>. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. </p>
+                </div>
+            </div>
+        </div>
+        <!-- end #content -->
+        <div id="sidebar">
+            <ul>
+                <li>
+                    <h2>Top-5 authors</h2>
+                    <ul>
+                        <li><a href="#">Sofia Shperling</a></li>
+                        <li><a href="#">Antonio Pablo</a></li>
+                        <li><a href="#">Ivan Pablo</a></li>
+                        <li><a href="#">Vasiliy Pablo</a></li>
+                        <li><a href="#">Daria Antioniella</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Top-5 posts</h2>
+                    <ul>
+                        <li><a href="#">Nec metus sed donec</a></li>
+                        <li><a href="#">Magna lacus bibendum mauris</a></li>
+                        <li><a href="#">Velit semper nisi molestie</a></li>
+                        <li><a href="#">Eget tempor eget nonummy</a></li>
+                        <li><a href="#">Nec metus sed donec</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- end #sidebar -->
+        <div style="clear: both;">&nbsp;</div>
+        <body>
+    </div>
+    <!-- end #page -->
