e48ec38bf3b71ef0bcf091b324d3add1b22c42d7
SofiaShperling <73811581+SofiaShperling@users.noreply.github.com>
2021-06-25 17:32:56 +0700

    Delete www directory

--- www/writersociety/Root/public/header.php
+++
@@ -1,8 +0,0 @@
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>Writer Society</title>
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-<div id="logo">
-    <h1><a href="#">Writer Society</a></h1>
-</div>
--- www/writersociety/Root/public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-include("mainpage.html");
-//include("content.php");
-//include("footer.php");
